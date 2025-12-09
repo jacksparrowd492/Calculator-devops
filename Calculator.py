8713b63cb3ac240762790d8b2b341452896cde93
SHANMUGABHARATHI A <50900@nlcads.nlcindia.com>
2025-12-09 12:00:59 +0530

    Add simple calculator script

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter the first number: "))
+operation = input("Enter operation (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 == 0:
+        result = "Error! Division by zero."
+    else:
+        result = num1 / num2
+else:
+    result = "Invalid operation!"
+
+print("Result:", result)
